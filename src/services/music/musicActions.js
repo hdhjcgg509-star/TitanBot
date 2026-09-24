1279aaee7fcc8283bd3925ba8cb0e89521cb7b27
hdhjcgg509-star <hdhjcgg509@gmail.com>
2026-09-24 10:12:46 +0300

    Change query type from 'ytsearch' to 'ytmsearch'

--- src/services/music/musicActions.js
+++ src/services/music/musicActions.js
@@ -207,7 +207,7 @@ export async function playQuery(client, interaction, query) {
     const { player, guildData } = await ensurePlayer(client, interaction);
 
     const result = await client.riffy.resolve({
-        query: 'ytsearch:' + query,
+        query: 'ytmsearch:' + query,
         requester: interaction.user,
     });
 
